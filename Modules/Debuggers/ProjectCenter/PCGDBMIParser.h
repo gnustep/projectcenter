fcc640f75b7ecec9b7163938fa2297acd72a54cf
Gregory John Casamento <greg.casamento@gmail.com>
2021-06-26 02:31:55 -0400

    Add parser class

---
+++ Modules/Debuggers/ProjectCenter/PCGDBMIParser.h
@@ -0,0 +1,42 @@
+/*
+**  PCGDBMIParser.h
+**
+**  Copyright (c) 2020 Free Software Foundation
+**
+**  Author: Gregory Casamento <greg.casamento@gmail.com>
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef _PCGDMIParser_h_INCLUDE
+#define _PCGDMIParser_h_INCLUDE
+
+#import <Foundation/NSObject.h>
+
+@class NSString;
+@class PCGDBMIRecord;
+
+@interface PCGDBMIParser : NSObject
+{
+  NSString *_string;
+}
+
+- (instancetype) initWithString: (NSString *)string;
+- (void) setString: (NSString *)string;
+- (PCGDBMIRecord *) parse;
+
+@end
+
+#endif
